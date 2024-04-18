6d7f0c18aacb37c74a3d1258490c850b77bc8d9c
dione.evangelista <dione.evangelista@lecom.com.br>
2024-04-18 19:34:52 -0300

    feat: created CategoriesRepository

--- server/src/model/Category.ts
+++ server/src/model/Category.ts
@@ -1,10 +1,11 @@
 import { randomUUID } from 'node:crypto'
 
+// Verificar essa dualidade de tipo para name, description e created_at
 export class Category {
   id?: string
-  name: string
-  description: string
-  created_at: Date
+  name: string | undefined
+  description: string | undefined
+  created_at: Date | undefined
 
   constructor() {
     if (!this.id) {
--- server/src/routes/category.routes.ts
+++ server/src/routes/category.routes.ts
@@ -1,20 +1,25 @@
 import { Router } from 'express'
-import { Category } from '../model/Category'
+import { CategoryRepository } from '../repositories/CategoriesRepository'
 
 export const categoryRoutes = Router()
-
-const categories: Category[] = []
+const categoriesRepository = new CategoryRepository()
 
 categoryRoutes.post('/', (req, res) => {
   const { name, description } = req.body
 
-  const category = new Category()
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  })
-  categories.push(category)
+  const categoryAlreadyExists = categoriesRepository.findByName(name)
 
-  return res.status(201).json({ category })
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: 'Category Already exists!' })
+  }
+
+  categoriesRepository.create({ name, description })
+
+  return res.status(201).send()
+})
+
+categoryRoutes.get('/', (req, res) => {
+  const all = categoriesRepository.list()
+
+  return res.json(all)
 })
