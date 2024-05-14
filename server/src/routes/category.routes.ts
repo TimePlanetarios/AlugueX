ffcd3ebad5717ccb4d8330f905d8a3f430413001
dione.evangelista <dione.evangelista@lecom.com.br>
2024-05-14 16:49:50 -0300

    :sparkles: feat: Import File Initial Implementation

--- server/src/routes/category.routes.ts
+++ server/src/routes/category.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import multer from 'multer'
 
 export const categoryRoutes = Router()
+
+const upload = multer({
+  dest: './tmp',
+})
 
 categoryRoutes.post('/', (req, res) => {
   return createCategoryController.handle(req, res)
@@ -10,4 +15,10 @@ categoryRoutes.post('/', (req, res) => {
 
 categoryRoutes.get('/', (req, res) => {
   return listCategoriesController.handle(req, res)
+})
+
+categoryRoutes.post('/import', upload.single('file'), (req, res) => {
+  const { file } = req
+  console.log(file)
+  return res.send()
 })
